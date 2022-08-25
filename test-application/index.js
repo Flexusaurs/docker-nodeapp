11b9e813aaebf848c02542a02535fc2ef271ea0a
Itay Flexusaurs <eitayhaim@gmail.com>
2022-08-25 04:28:41 +0300

    commiting project

---
+++ test-application/index.js
@@ -0,0 +1,26 @@
+import address from "address";
+import Express from 'express';
+
+const app = Express()
+const appId = process.env.APPID
+console.log(appId)
+//grabbing local ip by specifying interface, family
+const connectionInterface = address.interface('IPv4');
+const localAddress = connectionInterface.address;
+//checking if the internal field in the interface object if true or false
+connectionInterface.internal === true ? console.error():
+console.log(connectionInterface.address)    
+app.listen(80, function(){
+    console.log("app running on port 80")
+})
+app.set('view engine', 'pug')
+
+app.get('/', (req,res) => {
+ res.render('index', {address: localAddress})
+    
+    
+})
+
+console.log(appId)
+
+
